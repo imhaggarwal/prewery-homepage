370e58285d7bf6ae374c5824cc175637c9748ce9
Peace89keeper <harshrawat2803@gmail.com>
2026-07-01 18:58:23 +0530

    deploy

--- src/document/TopBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const TopBar = () => (
-  <div className="top-bar">
-    <span>🔥 Register with Prewery & connect with brands. <a href="#register">REGISTER</a></span>
-  </div>
-);
-
-export default TopBar;
